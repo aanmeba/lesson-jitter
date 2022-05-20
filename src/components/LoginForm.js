e9d91ce51c319023262a20b6f107bf3fdc6df19c
aanmeba <aanmeba@gmail.com>
2022-05-20 12:18:45 +0900

    day 1 lesson, hooks and forms

---
+++ src/components/LoginForm.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+const LoginForm = ({ activateUser }) => {
+  const initialFormData = {
+    user: "",
+    password: "",
+  };
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("you clicked submit");
+    console.log(formData);
+
+    activateUser(formData.user);
+    setFormData(initialFormData); // cleaning up the input field
+  };
+
+  const handleFormData = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username: </label>
+          <input
+            type="text"
+            name="user"
+            id="user"
+            value={formData.user}
+            onChange={handleFormData}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={formData.password}
+            onChange={handleFormData}
+          />
+        </div>
+        <div>
+          <input type="submit" value="Login" />
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default LoginForm;
---
+++ src/components/MessageForm.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const MessageForm = ({ logginedUser, addMessage }) => {
+  const initialFormData = {
+    text: "",
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleFormData = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.text === "") {
+      console.log("empty message");
+    } else {
+      console.log(formData);
+      addMessage(formData.text);
+      cleanMessage();
+    }
+  };
+
+  const cleanMessage = (e) => {
+    // e.preventDefault();
+    setFormData(initialFormData);
+  };
+
+  return (
+    <>
+      <p></p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <textarea
+            type="text"
+            name="text"
+            id="text"
+            placeholder={`What's on your mind ${logginedUser}`}
+            value={formData.text}
+            onChange={handleFormData}
+          ></textarea>
+        </div>
+        <input type="submit" value="Post" />
+        <button onClick={cleanMessage}>Clear</button>
+      </form>
+    </>
+  );
+};
+
+export default MessageForm;
